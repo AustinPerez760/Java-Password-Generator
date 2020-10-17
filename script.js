bf2d94d3927d5f01b576de6d48ef93171e0b9b3a
Austin Perez <austin.perez760@gmail.com>
2020-10-17 00:37:29 -0700

    potential functions to be used to generate necessary random characters and numbers

--- script.js
+++ script.js
@@ -12,3 +12,44 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+//multiple functions needed to generate random uppercase & lowercase letters
+//multiple funtions needed to generate random numebers and symbols
+//must return string of characters
+//need whole numbers not decimals 
+//browser character set can incorporate symbols and numbers
+
+
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97); 
+
+}
+
+console.log(getRandomLower());
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65); 
+
+}
+
+console.log(getRandomUpper());
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48); 
+
+}
+
+console.log(getRandomNumber());
+
+function getRandomSymbol() {
+    const symbols = "*&$#!@?/.,=+";
+    return symbols [Math.floor ]
+
+}
+
+console.log(getRandomSymbol());
